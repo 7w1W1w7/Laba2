b4e8d761399aa47977652285007786934a6cbdd4
7w1W1w7 <74020936+7w1W1w7@users.noreply.github.com>
2020-11-06 21:37:33 +0300

    Add files via upload

---
+++ Laba2.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+    int N, control = 0;
+    char arr[1000];
+    char a;
+    int i = 0, j;
+    printf("Input a string:\n");
+    while ((a = getchar()) != '\n')
+    {
+        arr[i] = a;
+        i++;
+    }
+    arr[i] = '\0';
+    printf("Input a word size:\n");
+    scanf_s("%d", &N);
+    i = 0;
+    while (1)
+    {
+        int m = 0;
+        while ((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z'))
+        {
+
+            i++;
+            m++;
+        }
+        if (m == N)
+        {
+
+            for (j = i - m; j < i; j++)
+            {
+                printf("%c", arr[j]);
+            }
+            printf(" ");
+        }
+        while (!((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z')))
+        {
+            if (arr[i] == '\0')
+            {
+                control = 1;
+                break;
+            }
+            i++;
+        }
+        if (control == 1)
+            break;
+    }
+    return 0;
+}
